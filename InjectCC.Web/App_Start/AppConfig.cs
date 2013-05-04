a5dfd87ee1443337c892f0280523462b014c33c6
Stuart Branham <glompix@gmail.com>
2013-05-03 23:50:49 -0400

    asdfasdf

---
+++ InjectCC.Web/App_Start/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace InjectCC.Web
+{
+    public class AppConfig
+    {
+        public ApplicationEnvironment Environment { get; private set; }
+
+        public static AppConfig _config = null;
+        public static void RegisterConfig()
+        {
+            _config = new AppConfig {
+                Environment = (ApplicationEnvironment)Enum.Parse(typeof(ApplicationEnvironment), ConfigurationManager.AppSettings["Environment"])
+            };
+        }
+    }
+
+    public enum ApplicationEnvironment { Development, Testing, Production }
+}
