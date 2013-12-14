2cf5a20ac1c082eccd023b2cb86cf63fbc56e92a
Stuart Branham <stuart.branham@yourcls.com>
2013-12-14 12:56:36 -0500

    slidfjisldjfilsjf (i am so sorry)

--- InjectCC.Web/App_Start/BundleConfig.cs
+++ InjectCC.Web/App_Start/BundleConfig.cs
@@ -60,22 +60,6 @@ namespace InjectCC.Web
 
             bundles.Add(new StyleBundle("~/Content/injection").Include(
                 "~/Content/bootstrap-datepicker*"));
-
-            /*
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                "~/Content/themes/base/jquery.ui.core.css",
-                "~/Content/themes/base/jquery.ui.resizable.css",
-                "~/Content/themes/base/jquery.ui.selectable.css",
-                "~/Content/themes/base/jquery.ui.accordion.css",
-                "~/Content/themes/base/jquery.ui.autocomplete.css",
-                "~/Content/themes/base/jquery.ui.button.css",
-                "~/Content/themes/base/jquery.ui.dialog.css",
-                "~/Content/themes/base/jquery.ui.slider.css",
-                "~/Content/themes/base/jquery.ui.tabs.css",
-                "~/Content/themes/base/jquery.ui.datepicker.css",
-                "~/Content/themes/base/jquery.ui.progressbar.css",
-                "~/Content/themes/base/jquery.ui.theme.css"));
-             */
         }
     }
 }
